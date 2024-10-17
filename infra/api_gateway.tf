5faaeff6811f0aeada3a5cf0fb2841165222da21
Eduardo Freitas <eduardoflorencio96@gmail.com>
2024-10-17 00:08:24 -0300

    feat: add POST method

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -9,25 +9,43 @@ resource "aws_api_gateway_resource" "resource" {
   path_part   = "example"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  http_method             = aws_api_gateway_method.get_method.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.example.invoke_arn
+}
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.post_method.http_method
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
   uri                     = aws_lambda_function.example.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.integration]
-
+  depends_on = [
+    aws_api_gateway_integration.get_integration,
+    aws_api_gateway_integration.post_integration
+  ]
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "dev"
 }
